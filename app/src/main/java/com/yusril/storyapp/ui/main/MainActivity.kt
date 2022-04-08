b20bf1103feaf68bda7e4793082c27afacbccc66
yysrll <yusril.uzumaki@gmail.com>
2022-04-08 22:43:23 +0800

    initial commit

---
+++ app/src/main/java/com/yusril/storyapp/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.yusril.storyapp.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.yusril.storyapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
