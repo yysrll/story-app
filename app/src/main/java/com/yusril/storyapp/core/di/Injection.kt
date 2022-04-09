09e74d26c08285bb78dbb991eea1aeb7062f32b9
yysrll <yusril.uzumaki@gmail.com>
2022-04-09 12:59:12 +0800

    fix: change concrete class DataRepository to interface IRespository

--- app/src/main/java/com/yusril/storyapp/core/di/Injection.kt
+++ app/src/main/java/com/yusril/storyapp/core/di/Injection.kt
@@ -4,11 +4,12 @@ import android.content.Context
 import com.yusril.storyapp.core.data.DataRepository
 import com.yusril.storyapp.core.data.remote.ApiConfig
 import com.yusril.storyapp.core.data.remote.RemoteDataSource
+import com.yusril.storyapp.core.domain.repository.IRepository
 import com.yusril.storyapp.core.domain.usecase.StoryInteractor
 import com.yusril.storyapp.core.domain.usecase.StoryUseCase
 
 object Injection {
-    fun provideRepository(context: Context) : DataRepository {
+    fun provideRepository(context: Context) : IRepository {
         val remoteDataSource = RemoteDataSource.getInstance(ApiConfig.getApiService())
 
         return DataRepository.getInstance(remoteDataSource)
--- app/src/main/java/com/yusril/storyapp/core/domain/usecase/StoryInteractor.kt
+++ app/src/main/java/com/yusril/storyapp/core/domain/usecase/StoryInteractor.kt
@@ -1,12 +1,12 @@
 package com.yusril.storyapp.core.domain.usecase
 
 import androidx.lifecycle.LiveData
-import com.yusril.storyapp.core.data.DataRepository
 import com.yusril.storyapp.core.data.remote.response.LoginResult
 import com.yusril.storyapp.core.data.remote.response.ResultResponse
+import com.yusril.storyapp.core.domain.repository.IRepository
 import com.yusril.storyapp.core.vo.Resource
 
-class StoryInteractor(private val repository: DataRepository) : StoryUseCase {
+class StoryInteractor(private val repository: IRepository) : StoryUseCase {
     override fun register(
         name: String,
         email: String,
