bab1168d3e06114c74bb86ef70a1620c010166b0
yysrll <yusril.uzumaki@gmail.com>
2022-04-29 11:16:25 +0800

    feat: get image from intent camera

--- app/src/main/java/com/yusril/storyapp/ui/createstory/CreateStoryActivity.kt
+++ app/src/main/java/com/yusril/storyapp/ui/createstory/CreateStoryActivity.kt
@@ -1,11 +1,23 @@
 package com.yusril.storyapp.ui.createstory
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.yusril.storyapp.R
 import com.yusril.storyapp.databinding.ActivityCreateStoryBinding
+import java.io.File
 
 class CreateStoryActivity : AppCompatActivity() {
 
@@ -20,6 +32,68 @@ class CreateStoryActivity : AppCompatActivity() {
             setDisplayHomeAsUpEnabled(true)
             title = getString(R.string.create_story_title)
         }
+
+        if (!allPermissionGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSION,
+                REQUEST_CODE_PERMISSION
+            )
+        }
+
+        binding.apply {
+            buttonCamera.setOnClickListener { startTakePhoto() }
+        }
+    }
+
+
+    private lateinit var currentPhotoPath: String
+    private fun startTakePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(packageManager)
+
+        createCustomTempFile(application).also {
+            val photoUri: Uri = FileProvider.getUriForFile(
+                this,
+                "com.yusril.storyapp",
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+            launcherIntentCamera.launch(intent)
+        }
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+            val result = BitmapFactory.decodeFile(myFile.path)
+            binding.imagePreview.setImageBitmap(result)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            if (!allPermissionGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionGranted() = REQUIRED_PERMISSION.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
 
@@ -29,6 +103,9 @@ class CreateStoryActivity : AppCompatActivity() {
     }
 
     companion object {
+        private val REQUIRED_PERMISSION = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSION = 10
+
         fun start(context: Activity) {
             val intent = Intent(context, CreateStoryActivity::class.java)
             context.startActivity(intent)
